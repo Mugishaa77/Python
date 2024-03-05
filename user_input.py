1be163c5ace0f8d2cb3def471f2bf6903d8458ee
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2024-03-05 11:49:31 +0300

    Fixed a bug in the print output

--- user_input.py
+++ user_input.py
@@ -1,11 +1,14 @@
 # Ask the user for their name
-name = input("What is your name? ")
+name = input("What is your name?: ")
+print (name)
 
 # Ask the user for their age
-age = input("How old are you? ")
+age = input("How old are you?: ")
+print(age)
 
 # Ask the user for their location
 location = input("Where do you live? ")
+print(location)
 
 # Print out a personalized message
-print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+print("Hello " + name + " you are " + age + " years old and live in " + location)
